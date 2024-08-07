0e5a00c62636c70561f70dd97181df72b37c7b63
Angel Cuevas <163451592+AngelCS94@users.noreply.github.com>
2024-08-07 12:57:08 +0000

    faltan retoques

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,14 +1,31 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
 import CardPlanetas from "./CardPlanetas";
 import CardPersonajes from "./CardPersonajes";
 import Cardstarships from "./CardStarships";
 
 export const Home = () => (
-  <div className="text-center mt-5">
-    <CardPlanetas />
-    <CardPersonajes />
-    <Cardstarships />
+  <div
+    className="text-center mt-5"
+    style={{
+      backgroundImage: "url('https://wallpapercave.com/wp/wp3172265.jpg')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      minHeight: "100vh",
+      padding: "20px",
+      color: "white"
+    }}
+  >
+    <div
+      className="container p-3"
+      style={{
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        borderRadius: "10px"
+      }}
+    >
+      <CardPlanetas />
+      <CardPersonajes />
+      <Cardstarships />
+    </div>
   </div>
 );
